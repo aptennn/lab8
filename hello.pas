6f24a469093869a6dbfea7e3f3ca65d6d2988d4f
aptennn <uegor@protonmail.com>
2023-09-30 10:32:56 +0300

    Add time

--- hello.pas
+++ hello.pas
@@ -1,3 +1,12 @@
 ﻿begin
-  print('Hello World');
+  var time := ReadInteger('Введите час от 0 до 23');
+  Assert((time >= 0) and (time <= 23));
+  
+  case time of
+    4..10: Print('Доброе утро, мир!');
+    11..16: Print('Добрый день, мир!');
+    17..22: Print('Добрый вечер, мир!');
+   else Print('Доброй ночи, мир!');
+  end;
+  
 end.
